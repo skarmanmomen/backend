e84cffe1f94b5f5d6467308b6263fe0ea97c286d
skarmanmomen <dgithub77@gmail.com>
2022-07-19 22:23:52 -0700

    app.js file updated

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ app.use(cors())
 //connect wtih server
 database()
 
+// just for demo 
+app.get("/", (req,res, next) => {
+    res.send("welcome to our api service")
+})
+
 // product route
 app.use("/api/v1",  productRoute)
 app.use("/api/v1", userRoute)
 app.use("/api/v1", orderRoute)
+
  
 // error handler 
 app.use(error)
